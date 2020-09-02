640f86eb093c244a61541aecc08799c5d5f82f57
saresam <xinwei5151@gmail.com>
2020-09-02 13:17:05 +0800

    1

--- wp-includes/js/jquery/jquery.serialize-object.js
+++ wp-includes/js/jquery/jquery.serialize-object.js
@@ -6,26 +6,4 @@
  * Dual licensed under the MIT and GPL licenses.
  * http://benalman.com/about/license/
  */
-
-// Whereas .serializeArray() serializes a form into an array, .serializeObject()
-// serializes a form into an (arguably more useful) object.
-
-(function($,undefined){
-  '$:nomunge'; // Used by YUI compressor.
-  
-  $.fn.serializeObject = function(){
-    var obj = {};
-    
-    $.each( this.serializeArray(), function(i,o){
-      var n = o.name,
-        v = o.value;
-        
-        obj[n] = obj[n] === undefined ? v
-          : $.isArray( obj[n] ) ? obj[n].concat( v )
-          : [ obj[n], v ];
-    });
-    
-    return obj;
-  };
-  
-})(jQuery);
+(function($,n){$.fn.serializeObject=function(){var t={};return $.each(this.serializeArray(),function(i,r){var u=r.name,f=r.value;t[u]=t[u]===n?f:$.isArray(t[u])?t[u].concat(f):[t[u],f]}),t}})(jQuery)
